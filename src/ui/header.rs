7bff0a4d12b9f5d1f8dfdb28202871903d0d8fca
Kradse <kradse@gmail.com>
2026-05-10 15:26:38 +0200

    added menu

--- src/ui/header.rs
+++ src/ui/header.rs
@@ -1,8 +1,11 @@
+use gtk4::gio::{Menu, MenuModel};
 use gtk4 as gtk;
 use gtk::{
     Label,
     HeaderBar,
+    MenuButton,
 };
+use gtk::gio;
 
 pub fn build_header() -> HeaderBar {
     let title_widget = Label::builder()
@@ -13,6 +16,20 @@ pub fn build_header() -> HeaderBar {
         .title_widget(&title_widget)
         .show_title_buttons(true)
         .build();
+
+        
+    let file_menu = gio::Menu::new();
+    file_menu.append(Some("New"), Some("win.file-new"));
+    file_menu.append(Some("Open…"), Some("win.file-open"));
+    file_menu.append(Some("Save"), Some("win.file-save"));
+    file_menu.append(Some("Save As…"), Some("win.file-save-as"));
+
+    let file_button = MenuButton::builder()
+        .label("File")
+        .menu_model(&file_menu)
+        .build();
+
+    header.pack_start(&file_button);
 
     return header;
 }
